15514cc48b562fd0f74ae92e79ca4ddf14c63f37
ipal <72238450+petelpop@users.noreply.github.com>
2024-05-23 11:31:30 +0700

    create textstyle template at theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,9 +1,30 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color purpleColor = const Color(0xFF5843BE);
 Color orangeColor = const Color(0xFFFF9376);
 Color blackColor = const Color(0xFF000000);
 Color whiteColor = const Color(0xFFFFFFFF);
 Color greyColor = const Color(0xFF82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+      fontWeight: FontWeight.w500,
+      color: blackColor
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+      fontWeight: FontWeight.w500,
+      color: whiteColor
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+      fontWeight: FontWeight.w300,
+      color: greyColor
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+      fontWeight: FontWeight.w500,
+      color: purpleColor
+);
