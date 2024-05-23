064cfc9f6584053d864ca4cd91a3d77f26b2994e
ipal <72238450+petelpop@users.noreply.github.com>
2024-05-23 11:21:06 +0700

    add google fonts package

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,8 +2,8 @@
 
 import 'package:flutter/material.dart';
 
-Color purpleColor = Color(0xFF5843BE);
-Color orangeColor = Color(0xFFFF9376);
-Color blackColor = Color(0xFF000000);
-Color whiteColor = Color(0xFFFFFFFF);
-Color greyColor = Color(0xFF82868E);
+Color purpleColor = const Color(0xFF5843BE);
+Color orangeColor = const Color(0xFFFF9376);
+Color blackColor = const Color(0xFF000000);
+Color whiteColor = const Color(0xFFFFFFFF);
+Color greyColor = const Color(0xFF82868E);
