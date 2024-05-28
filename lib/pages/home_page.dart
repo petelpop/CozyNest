b383a8b88e466f1c012c9e04aab4289b6e8a1798
ipal <72238450+petelpop@users.noreply.github.com>
2024-05-28 11:33:59 +0700

    create recommended space list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:cozy/models/City.dart';
+import 'package:cozy/models/Space.dart';
 import 'package:cozy/theme.dart';
 import 'package:cozy/widgets/city_card.dart';
+import 'package:cozy/widgets/space_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -11,10 +13,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+        bottom: false,
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 24),
           child: ListView(
             children: [
+
+
+              // NOTE: TITLE/HEADER
               Padding(
                 padding: EdgeInsets.only(left: 24),
                 child: Text(
@@ -34,34 +40,91 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 ),
-                SizedBox(height: 30),
-                Padding(
-                padding: EdgeInsets.only(left: 24),
-                child: Text(
-                  'Popular Cities',
-                  style: regularTextStyle.copyWith(
-                    fontSize: 16
-                  ),
-                ),
-                ),
-                SizedBox(height: 16),
-                Container(
-                  padding: EdgeInsets.only(left: 14),
-                  height: 150,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      CityCard(city: City(id: 1, image: 'assets/city1.png', text: 'Jakarta')),
-                      CityCard(city: City(id: 2, image: 'assets/city2.png', text: 'bandung', isPopular: true)),
-                      CityCard(city: City(id: 3, image: 'assets/city3.png', text: 'Surabaya'))
-                    ],
-                  ),
-                )
 
+                SizedBox(height: 30),
+                // NOTE: POPULAR CITIES
+                _titlePopularCities(),
+                SizedBox(height: 16),
+                _listPopularCities(),
+                SizedBox(height: 30),
+                //NOTE:RECOMMENDED SPACE
+                _titleSpace(),
+                SizedBox(height: 16),
+                _listSpace(),
+                SizedBox(height: 30),
+  
+                //NOTE: TIPS & GUIDANCE
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Text(
+                    'Tips & Guidance',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 16
+                    ),
+                  ),
+                  )
             ],
           ),
         )
         ),
     );
+  }
+
+  Padding _titlePopularCities() {
+    return Padding(
+              padding: EdgeInsets.only(left: 24),
+              child: Text(
+                'Popular Cities',
+                style: regularTextStyle.copyWith(
+                  fontSize: 16
+                ),
+              ),
+              );
+  }
+
+  Container _listPopularCities() {
+    return Container(
+                padding: EdgeInsets.only(left: 14),
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    CityCard(city: City(id: 1, image: 'assets/city1.png', text: 'Jakarta')),
+                    CityCard(city: City(id: 2, image: 'assets/city2.png', text: 'bandung', isPopular: true)),
+                    CityCard(city: City(id: 3, image: 'assets/city3.png', text: 'Surabaya'))
+                  ],
+                ),
+              );
+  }
+
+  Padding _listSpace() {
+    return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    children: [
+                      SpaceCard(
+                        space: Space(id: 1, name: 'Kuretakeso Hott', imageUrl: 'assets/space1.png', price: 52, city: 'Bandung', country: 'Indonesia', rating: 4),
+                      ),
+                      SizedBox(height: 29),
+                      SpaceCard(
+                        space: Space(id: 2, name: 'Roemah Nenek', imageUrl: 'assets/space2.png', price: 11, city: 'Bogor', country: 'Indonesia', rating: 5)
+                        ),
+                      SizedBox(height: 29),
+                      SpaceCard(space: Space(id: 3, name: 'Darrling How', imageUrl: 'assets/space3.png', price: 20, city: 'Jakarta', country: 'Indonesia', rating: 3)),
+                    ],
+                  ),
+                );
+  }
+
+  Padding _titleSpace() {
+    return Padding(
+                padding: EdgeInsets.only(left: 24),
+                child: Text(
+                  'Recommended Space',
+                  style: regularTextStyle.copyWith(
+                    fontSize: 16
+                  ),
+                ),
+                );
   }
 }
--- lib/widgets/city_card.dart
+++ lib/widgets/city_card.dart
@@ -55,7 +55,7 @@ class CityCard extends StatelessWidget {
                   style: blackTextStyle.copyWith(
                     fontSize: 16,
                   ),
-                  )
+                )
             ],
           ),
         ),
