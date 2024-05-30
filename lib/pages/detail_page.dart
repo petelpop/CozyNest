5cb84390a1efbbc11aeecd6c725f74fa5729d1d5
ipal <72238450+petelpop@users.noreply.github.com>
2024-05-30 08:06:05 +0700

    finish detail page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -10,7 +10,6 @@ class DetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          bottom: false,
           top: false,
           child: Stack(
             children: [
@@ -20,20 +19,54 @@ class DetailPage extends StatelessWidget {
                 height: 350,
                 fit: BoxFit.cover,
               ),
+              ListView(
+                children: [
+                  const SizedBox(height: 300),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: whiteColor),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 30),
+                        _titleDetailPage(),
+                        const SizedBox(height: 30),
+                        _titleFacilities(),
+                        const SizedBox(height: 12),
+                        _mainFacilities(),
+                        const SizedBox(height: 29),
+                        _titlePhotos(),
+                        const SizedBox(height: 12),
+                        _listPhotos(),
+                        SizedBox(height: 30),
+                        _locationTitle(),
+                          SizedBox(height: 6),
+                          _locationDetail(),
+                          SizedBox(height: 40),
+                          _buttonBook(context),
+                            SizedBox(height: 40)
+                      ],
+                    ),
+                  )
+                ],
+              ),
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
                       child: Image.asset(
                         'assets/btn_back.png',
                         width: 40,
                         height: 40,
                       ),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
                     ),
                     Image.asset(
                       'assets/btn_wishlist.png',
@@ -43,56 +76,142 @@ class DetailPage extends StatelessWidget {
                   ],
                 ),
               ),
-              ListView(
-                children: [
-                  SizedBox(height: 300),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: whiteColor),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 30),
-                        _titleDetailPage(),
-                        SizedBox(height: 30),
-                        Padding(
-                          padding: EdgeInsets.only(left: 24),
-                          child: Text(
-                            'Main Facilities',
-                            style: regularTextStyle.copyWith(
-                              fontSize: 16
-                              ),
-                          ),
-                        ),
-                        SizedBox(height: 12),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 24
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              FacilityItem(name: 'Kitchen', total: 2, imageUrl: 'assets/icon_kitchen.png'),
-                              FacilityItem(name: 'Bedroom', total: 3, imageUrl: 'assets/icon_bedroom.png'),
-                              FacilityItem(name: 'Big Cupboard', total: 3, imageUrl: 'assets/icon_cupboard.png'),
-                            ],
-                          ),
-                          )
-                      ],
-                    ),
-                  )
-                ],
-              )
             ],
           )),
     );
   }
 
+  Row _buttonBook(BuildContext context) {
+    return Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              onPressed: (){}, 
+                              child: Text(
+                                'Book Now',
+                                style: whiteTextStyle.copyWith(
+                                  fontSize: 18
+                                ),
+                                ),
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: Size(MediaQuery.of(context).size.width - 24*2, 50),
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
+                                backgroundColor: purpleColor
+                              ),
+                              ),
+                          ],
+                        );
+  }
+
+  Padding _locationDetail() {
+    return Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 24),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Jln. Kappan Sukses No. 20\nPalembang',
+                                style: greyTextStyle.copyWith(
+                                  fontSize: 14
+                                ),
+                                ),
+                                Image.asset(
+                                  'assets/btn_map.png',
+                                  width: 40,
+                                  height: 40
+                                  ),
+                            ],
+                          ),
+                        );
+  }
+
+  Padding _locationTitle() {
+    return Padding(
+                        padding: EdgeInsets.only(left: 24),
+                        child: Text(
+                          'Location',
+                          style: regularTextStyle.copyWith(
+                            fontSize: 16
+                          ),
+                        ),
+                        );
+  }
+
+  Container _listPhotos() {
+    return Container(
+                        height: 88,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            const SizedBox(width: 24),
+                            Image.asset(
+                              'assets/photo1.png',
+                              width: 110,
+                              height: 88,
+                              fit: BoxFit.cover,
+                            ),
+                            const SizedBox(width: 18),
+                            Image.asset(
+                              'assets/photo2.png',
+                              width: 110,
+                              height: 88,
+                              fit: BoxFit.cover,
+                            ),
+                            const SizedBox(width: 18),
+                            Image.asset(
+                              'assets/photo3.png',
+                              width: 110,
+                              height: 88,
+                              fit: BoxFit.cover,
+                            ),
+                            const SizedBox(width: 24),
+                          ],
+                        ),
+                      );
+  }
+
+  Padding _titlePhotos() {
+    return Padding(
+                        padding: const EdgeInsets.only(left: 24),
+                        child: Text(
+                          'Photos',
+                          style: blackTextStyle.copyWith(fontSize: 16),
+                        ),
+                      );
+  }
+
+  Padding _titleFacilities() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 24),
+      child: Text(
+        'Main Facilities',
+        style: regularTextStyle.copyWith(fontSize: 16),
+      ),
+    );
+  }
+
+  Padding _mainFacilities() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          FacilityItem(
+              name: 'Kitchen', total: 2, imageUrl: 'assets/icon_kitchen.png'),
+          FacilityItem(
+              name: 'Bedroom', total: 3, imageUrl: 'assets/icon_bedroom.png'),
+          FacilityItem(
+              name: 'Big Cupboard',
+              total: 3,
+              imageUrl: 'assets/icon_cupboard.png'),
+        ],
+      ),
+    );
+  }
+
   Padding _titleDetailPage() {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -101,7 +220,7 @@ class DetailPage extends StatelessWidget {
               'Kuretakeso Hott',
               style: blackTextStyle.copyWith(fontSize: 22),
             ),
-            SizedBox(height: 2),
+            const SizedBox(height: 2),
             Text.rich(
               TextSpan(
                 text: '\$52',
@@ -121,28 +240,28 @@ class DetailPage extends StatelessWidget {
               'assets/icon_star.png',
               width: 20,
             ),
-            SizedBox(width: 2),
+            const SizedBox(width: 2),
             Image.asset(
               'assets/icon_star.png',
               width: 20,
             ),
-            SizedBox(width: 2),
+            const SizedBox(width: 2),
             Image.asset(
               'assets/icon_star.png',
               width: 20,
             ),
-            SizedBox(width: 2),
+            const SizedBox(width: 2),
             Image.asset(
               'assets/icon_star.png',
               width: 20,
             ),
-            SizedBox(width: 2),
+            const SizedBox(width: 2),
             Image.asset(
               'assets/icon_star.png',
               width: 20,
-              color: Color(0xFF989BA1),
+              color: const Color(0xFF989BA1),
             ),
-            SizedBox(width: 22),
+            const SizedBox(width: 22),
           ],
         )
       ]),
